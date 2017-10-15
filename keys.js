c29ea2de1214f674d221714b8e0a96bf145adee0
Minu James <minujames@Minus-MBP.attlocal.net>
2017-10-14 23:18:04 -0700

    adding initial files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '1mbCnWO7u48QB9413cbN2mepn',
+  consumer_secret: 'vaoAa2HLr9bvWB3Twq0Zblk13E8wlrREHeX7MhLaRtzIwLJDic',
+  access_token_key: '919413524840955905-O2sLcDqghKicNxeXd3DxDFz23MhbwYw',
+  access_token_secret: 'nJfg5bKGlLy6jy7UvP0ag1qEH35hwbHc9uhBJydjcPshh',
+}
+
+module.exports = twitterKeys;
